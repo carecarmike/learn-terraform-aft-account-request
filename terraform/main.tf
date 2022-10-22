94ca08a7768db7c847bb132fbcf584d86ef58641
Mike Matz <mm@carecar.co>
2022-10-22 18:37:16 +0000

    Configuring first account request: sandbox-learn-aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "it-admin+aws-sandbox-learn-aft@carecar.co"
+    AccountName               = "sandbox-learn-aft"
+    ManagedOrganizationalUnit = "Learn AFT (ou-j4fl-cezgyf0y)"
+    SSOUserEmail              = "it-admin+aws-learn-aft@carecar.co"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
